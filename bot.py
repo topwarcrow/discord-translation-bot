153f26a290f85719321e1b090ee09f48950b988d
ogikubokazuyuki <cytgne@yunoMacBook.local>
2025-04-25 05:08:41 +0900

    Clean commit without secrets

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from googletrans import Translator
+
+TOKEN = "MTM2NDk2NjEzMzE5MDM2NTIxNA.GFOGx2.9re3i4-whBs_JsboQp_Dxc5S-MMBXagiwizlOs"
+
+JAPANESE_CHANNEL_ID = 1364961133940838512
+ENGLISH_CHANNEL_ID = 1364960893493973092
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+translator = Translator()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} 起動完了！")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.channel.id == JAPANESE_CHANNEL_ID:
+        result = translator.translate(message.content, src="ja", dest="en")
+        target_channel = bot.get_channel(ENGLISH_CHANNEL_ID)
+        await target_channel.send(f"{message.author.display_name}: {result.text}")
+
+    elif message.channel.id == ENGLISH_CHANNEL_ID:
+        result = translator.translate(message.content, src="en", dest="ja")
+        target_channel = bot.get_channel(JAPANESE_CHANNEL_ID)
+        await target_channel.send(f"{message.author.display_name}: {result.text}")
+
+    await bot.process_commands(message)
+
+bot.run(TOKEN)
